9c9cc23ac052336e1156d9d8ebacceeec885752d
challaramakrishnareddy52 <challaramakrishnareddy52@gmail.com>
2022-11-14 22:59:09 +0530

    SnakeandLadderUC3

--- SnakeandLadder/Program.cs
+++ SnakeandLadder/Program.cs
@@ -1,20 +1,44 @@
-﻿namespace SnakeandLadder
+﻿using System.Security.Cryptography.X509Certificates;
+
+namespace SnakeandLadder
 {
     class Program
     {
+        private const int V = 0;
+
         static void Main(string[] args)
         {
-            int Pos = 0;
+            int startPos = 0;
             int numofPlayers = 0;
-            Console.WriteLine("single player at start position :" +Pos);
-            RollDice();
-        }
-        public static void RollDice()
-        {
-            Random random= new Random();
-            int dice = random.Next(3);
-            dice = dice + 1;
-            Console.WriteLine("Dice Num :" + dice);
+            int currPos = 0;
+            int diceVal = 0;
+
+
+             static void RollDice(int numofPlayers)
+             {
+                Console.WriteLine("Enter Number Of Players");
+                numofPlayers = Convert.ToInt32(Console.ReadLine());
+
+                Random random = new Random();
+                int dice = random.Next(1, 7);
+                Console.WriteLine("Dice Num :" + dice);
+
+                Random randomNum = new Random();
+                int option = random.Next(1, 7);
+                switch (option) 
+                {
+                    case 0:
+                        Console.WriteLine("ladder");
+                        break;
+                    case 1:
+                        Console.WriteLine("Snake");
+                        break;
+                    default:
+                        Console.WriteLine("No Play");
+                        break;
+                }
+            }
         }
     }
 }
+ 
