2ca1874b57837d064a2596f0180b7b22a6fb97d6
challaramakrishnareddy52 <challaramakrishnareddy52@gmail.com>
2022-11-14 09:42:17 +0530

    Snake and Ladder

---
+++ SnakeandLadder/Program.cs
@@ -0,0 +1,12 @@
+﻿namespace SnakeandLadder
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int startPos = 0;
+            int numofPlayers = 0;
+            Console.WriteLine("single player at start position ");
+        }
+    }
+}
