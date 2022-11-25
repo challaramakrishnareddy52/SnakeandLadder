a5777fed9e8c7ffa88a50cadfbe44c5352305c09
challaramakrishnareddy52 <challaramakrishnareddy52@gmail.com>
2022-11-25 17:50:24 +0530

    Random to get the Number Between 1 to 6

--- SnakeandLadder/Program.cs
+++ SnakeandLadder/Program.cs
@@ -5,15 +5,13 @@
         static void Main(string[] args)
         {
             int Pos = 0;
-            int numofPlayers = 0;
-            Console.WriteLine("single player at start position :" +Pos);
+            Console.WriteLine("single player at start position :" + Pos);
             RollDice();
         }
-        public static void RollDice()
+         public static void RollDice()
         {
             Random random= new Random();
             int dice = random.Next(3);
-            dice = dice + 1;
             Console.WriteLine("Dice Num :" + dice);
         }
     }
