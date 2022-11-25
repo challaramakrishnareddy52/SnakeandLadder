014586976a45f215d1c6cf44f75b7ab3f3044b81
challa ramakrishna reddy <114689103+challaramakrishnareddy52@users.noreply.github.com>
2022-11-25 18:11:20 +0530

    Update Program.cs

--- SnakeandLadder/Program.cs
+++ SnakeandLadder/Program.cs
@@ -11,12 +11,8 @@
          public static void RollDice()
         {
             Random random= new Random();
-<<<<<<< HEAD
             int dice = random.Next(3);
-=======
             int dice = random.Next(1, 7);
-            dice = dice + 1;
->>>>>>> 98a3fdbac5a4ab7b2aa09101bfa4c68b64d04822
             Console.WriteLine("Dice Num :" + dice);
         }
     }
